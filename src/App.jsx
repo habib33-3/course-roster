fdd4a2c0d132bbfd909d3628756ef06f1260ec45
Habibur Rahman <habib.swi16@gmail.com>
2023-09-14 22:45:47 +0600

    static card design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import "./App.css";
+import Card from "./assets/components/Card/Card";
 
-function App() {}
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-black font-bold text-center mb-12">
+        Course Registration
+      </h1>
+
+      <Card></Card>
+    </>
+  );
+}
 
 export default App;
---
+++ src/assets/components/Cards/Cards.jsx
@@ -0,0 +1,13 @@
+
+import PropTypes from 'prop-types'
+import Card from '../Card/Card'
+
+const Cards = props => {
+  return (
+   <Card></Card>
+  )
+}
+
+Cards.propTypes = {}
+
+export default Cards
